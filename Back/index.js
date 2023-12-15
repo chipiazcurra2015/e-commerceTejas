4e48821648fd534704c614ecd57aeae34e547aca
chipiazcurra2015 <chipiazcurra2015@gmail.com>
2023-12-15 11:53:55 -0300

    levantoServer

---
+++ Back/index.js
@@ -0,0 +1,14 @@
+const axios = require  ('axios');
+const server = require  ('./src/server.js');
+const { conn } = require('./src/db.js');
+
+const PORT = 3001;
+
+// alter - force
+conn.sync({ alter: true })
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.error(error));
---
+++ Back/src/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+// const router = require('../../Back/index');
+const morgan = require("morgan");
+const cors = require("cors");
+
+const server = express();
+
+server.use(cors()); //Permite que cualquier cliente se conecte 
+
+server.use(morgan("dev")); //Registra tus peticiones en la terminal.
+server.use((req, res, next)=> {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+        "Access-Control-Allow-Headers", 
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+}); // Para que mi servidor no se rompa al conectar con el browser.
+
+server.use(express.json()); //Funcion de parseo que detecta que es un json.
+
+// server.use(router); //Cualquier solicitud a la ruta raíz del servidor seran manejada por las rutas definidas en routes.
+
+
+module.exports = server;
